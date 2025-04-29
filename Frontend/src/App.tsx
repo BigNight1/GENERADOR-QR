711a2898500dcb5cebaa479a7ecb52d8c3fbb272
BigNight <edu_armas11@hotmail.com>
2025-04-29 00:15:29 -0500

    mmmm

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch('https://generador-qr-production.up.railway.app/', {
+      const response = await fetch('https://generador-qr-production.up.railway.app/generate-qr', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
