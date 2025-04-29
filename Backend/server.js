4f634b642addc658a123d958609eb0b69a745afa
BigNight <edu_armas11@hotmail.com>
2025-04-28 23:40:16 -0500

    change backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,10 @@ import QRCode from "qrcode";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://generador-qr-three.vercel.app', // Tu frontend
+  methods: ['GET', 'POST'], // Métodos permitidos
+}));
 app.use(express.json());
 
 // Endpoint para generar código QR
